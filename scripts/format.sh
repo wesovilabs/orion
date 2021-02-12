97236e6e00fb9489a6fd7f031c60e76f4383e46b
Iván Corrales Solera <ivan.corrales.solera@gmail.com>
2021-02-12 07:53:40 +0100

    fix: fix issue in  script format

--- scripts/format.sh
+++ scripts/format.sh
@@ -8,5 +8,5 @@ DIR="$( cd -P "$( dirname "$SOURCE" )/.." && pwd )"
 cd "$DIR"
 
 for pkg in $(GOFLAGS=-mod=vendor go list -f '{{.Dir}}' ./... | grep -v /vendor/ ); do \
-    GOFLAGS=-mod=vendor go run -mod=vendor golang.org/x/tools/cmd/goimports -local -l -w -e $$pkg/*.go; \
+    GOFLAGS=-mod=vendor go run -mod=vendor golang.org/x/tools/cmd/goimports -local -l -w -e $pkg/*.go; \
 done
